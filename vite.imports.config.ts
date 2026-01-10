92a50b0ad3b42548a6045738ad56e21c63670c41
fanslar <fanckingg@gmail.com>
2026-01-10 23:51:54 +0800

    fix: import-map css auto import

--- vite.imports.config.ts
+++ vite.imports.config.ts
@@ -1,6 +1,26 @@
 import { defineConfig, type UserConfig } from 'vite'
+import path from 'node:path'
+import fs from 'node:fs'
+/**
+ * Patch generated entries and import their corresponding CSS files.
+ * Also normalize MonacoEditor.css
+ */
+const patchCssFiles: Plugin = {
+  name: 'imports-patch-css',
+  apply: 'build',
+  writeBundle() {
+    //  inject css imports to the files
+    const outDir = path.resolve('dist', 'imports')
+    ;['hljs', 'markdown-it'].forEach((file) => {
+      const filePath = path.resolve(outDir, file + '.js')
+      const content = fs.readFileSync(filePath, 'utf-8')
+      fs.writeFileSync(filePath, `import './imports.css'\n${content}`)
+    })
+  },
+}
 
 export default defineConfig({
+  plugins: [patchCssFiles],
   build: {
     target: 'esnext',
     outDir: 'dist/imports',
@@ -14,5 +34,12 @@ export default defineConfig({
       formats: ['es'],
       fileName: () => '[name].js',
     },
+    rollupOptions: {
+      output: {
+        assetFileNames(info) {
+          return 'imports.css'
+        }
+      }
+    }
   }
 })
